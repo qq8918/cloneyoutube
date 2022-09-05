fb1583be7291f1eb5cfe3fdb511d48c5fe6f2096
qq8918 <qq8918@naver.com>
2022-09-05 14:54:16 +0900

    두번째 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Youtube'),
+        title: Text('Yotube'),
       ),
     );
   }
